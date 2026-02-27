34b10e2c85fc984585ef5ea67b5a4d0739c224e5
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-02-28 06:29:41 +0700

    Create PhpSahur.php

---
+++ src/PhpSahur.php
@@ -0,0 +1,16 @@
+<?php
+   namespace PhpSahur
+
+   class PhpSahur
+   {
+    public function jelaskan()
+    {
+      echo "\e[1;34mHalo!, ini adalah library php-sahur sangat simpel yang dibuat di bulan Suci Ramadhan 😁.\e[0m" ."\n"; 
+    }
+     
+    public function sahur()
+    {
+       return "\e[33mSahurr!!!\e[0m"."\n";
+    }
+   }
+?>
