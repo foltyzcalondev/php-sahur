8d18a2750ff06270698cb941e98e128f3b17cc3d
FoltyzTheDev <foltyzcalondev@gmail.com>
2026-02-28 07:12:56 +0700

    Update PhpSahur.php

--- src/PhpSahur.php
+++ src/PhpSahur.php
@@ -1,15 +1,12 @@
 <?php
-   namespace PhpSahur
+   namespace PhpSahur;
 
-   class PhpSahur
-   {
-    public function jelaskan()
-    {
+   class PhpSahur {
+    public function jelaskan() {
       echo "\e[1;34mHalo!, ini adalah library php-sahur sangat simpel yang dibuat di bulan Suci Ramadhan 😁.\e[0m" ."\n"; 
     }
      
-    public function sahur()
-    {
+    public function sahur() {
        return "Sahurrr!!!";
     }
    }
